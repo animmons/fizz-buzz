a197e61fee393d0a7b83a2f285358b8c8d36cce6
Alex <alex.nimmons@gmail.com>
2014-10-17 11:11:32 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+
+for (i = 0; i < 100; i++){
+  if (i % 3 == 0){
+  console.log('fizz');
+  } else { 
+  console.log(i);
+  }
+}
+
+
+
+
+
+/*
+
+	var x = (i%3=0)
+	var y = (i%5=0)
+
+	if (i == x) {
+		console.log ("fizz");
+	}
+
+	else if (i == y) {
+		console.log ("buzz");
+	}
+
+	else {
+		console.log(i);
+	}
+	}
+
+
+*/
